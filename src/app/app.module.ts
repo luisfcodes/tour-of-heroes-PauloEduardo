efd6317dff31aa8e26f3843f7e23daf97a22a0bf
Luis <luis.silva9902@gmail.com>
2023-02-02 14:20:12 -0300

    Created core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,23 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MessagesComponent } from './messages/messages.component'
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { DashboardComponent } from './dashboard/dashboard.component'
-import { MaterialModule } from './material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     AppRoutingModule,
-    MaterialModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
